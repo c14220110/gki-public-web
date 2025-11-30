61bc7492e0dc5a9feecf25684964554395fbc331
c14220110 <c14220110@john.petra.ac.id>
2025-11-30 23:12:53 +0700

    jelaskan singkat perubahannya

--- js/ai-voice.js
+++ js/ai-voice.js
@@ -285,7 +285,7 @@ async function startSession() {
 
     // 5. Buka sesi realtime
     sessionPromise = aiClient.live.connect({
-      model: "gemini-live-2.5-flash-preview-native-audio-09-2025",
+      model: "gemini-2.5-flash-native-audio-preview-09-2025",
       config: {
         responseModalities: [Modality.AUDIO],
         systemInstruction,
